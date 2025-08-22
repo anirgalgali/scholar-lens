added normalization layers and fixed minor bugs

src/mintransformer/layers.py
@@ -4,7 +4,14 @@ import math
 
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -31,20 +38,22 @@ class Linear(nn.Module):
         torch.nn.init.trunc_normal_(
             self.weight, 0.0, std=init_std, a=-3 * init_std, b=3 * init_std
         )
-        if self.bias:
+        if self.bias is not None:
             torch.nn.init.trunc_normal_(
                 self.bias, mean=0, std=init_std, a=-3 * init_std, b=3 * init_std
             )
 
     def forward(self, input):  # input is [batch_dims, dim]
-        if self.bias:
+        if self.bias is not None:
             return input @ self.weight.T + self.bias
         return input @ self.weight.T
 
 
 class Embedding(nn.Module):
 
-    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+    def __init__(
+        self, num_embeddings: int, embedding_dim: int, device=None, dtype=None
+    ):
         super().__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
@@ -56,9 +65,85 @@ class Embedding(nn.Module):
             ),
             requires_grad=True,
         )
+        self._initialize()
 
     def _initialize(self):
         torch.nn.init.trunc_normal_(self.weight, 0.0, std=1.0, a=-3, b=3)
 
     def forward(self, token_ids):
         return self.weight[token_ids]
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(
+        self,
+        d_hidden: int,
+        eps: float = 1e-5,
+        bias: bool = True,
+        device=None,
+        dtype=None,
+    ):
+        super().__init__()
+        self.d_hidden = d_hidden
+        self.device = device
+        self.dtype = dtype
+        self.eps = eps
+        # calling what's typically gamma as 'weight' to be consistent with PyTorch definitions
+        self.weight = nn.Parameter(
+            torch.empty((d_hidden,), device=self.device, dtype=self.dtype),
+            requires_grad=True,
+        )
+        # calling what's typically beta as 'bias' to be consistent with PyTorch definitions
+        if bias:
+            self.bias = nn.Parameter(
+                torch.empty((d_hidden,), device=self.device, dtype=self.dtype),
+                requires_grad=True,
+            )
+        else:
+            self.register_parameter("bias", None)
+
+        self._initialize()
+
+    def _initialize(self):
+        torch.nn.init.ones_(self.weight)
+        if self.bias is not None:
+            torch.nn.init.zeros_(self.bias)
+
+    def forward(self, input):  # input is of size batch x n_seq x d_hidden
+        in_dtype = input.dtype  # IN case you are using mixed precisiion
+        input = input.to(torch.float32)
+        input_mean = torch.mean(input, dim=-1, keepdim=True)
+        centered_input = input - input_mean
+        input_var = torch.mean(centered_input**2, dim=-1, keepdim=True) + self.eps
+        scaled_centered_input = centered_input / torch.sqrt(input_var)
+        output = scaled_centered_input * self.weight[None, None, :]
+        if self.bias is not None:
+            output = output + self.bias[None, None, :]
+
+        return output.to(in_dtype)
+
+
+class RMSNorm(nn.Module):
+
+    def __init__(self, d_hidden: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.d_hidden = d_hidden
+        self.device = device
+        self.dtype = dtype
+        self.eps = eps
+        self.weight = nn.Parameter(
+            torch.empty((d_hidden,), device=self.device, dtype=self.dtype),
+            requires_grad=True,
+        )
+        self._initialize()
+
+    def _initialize(self):
+        torch.nn.init.ones_(self.weight)
+
+    def forward(self, input):  # input is of size batch x n_seq x d_hidden
+        in_dtype = input.dtype
+        input.to(torch.float32)
+        rms = torch.sqrt(torch.mean(input**2, dim=-1, keepdim=True) + self.eps)
+        output = (input / rms) * self.weight[None, None, :]
+        return output.to(in_dtype)

src/mintransformer/tests/test_model.py
@@ -2,8 +2,7 @@ from einops import rearrange
 import numpy
 import torch
 import torch.nn.functional as F
-from mintransformer.layers import Linear
-from mintransformer.layers import Embedding
+from mintransformer.layers import Linear, Embedding, RMSNorm, LayerNorm
 
 
 def test_linear(numpy_snapshot, ts_state_dict, in_embeddings, d_model, d_ff):
@@ -21,4 +20,29 @@ def test_embedding(numpy_snapshot, ts_state_dict, in_indices, vocab_size, d_mode
     output = embedding_layer(in_indices)
     numpy_snapshot.assert_match(output)
 
+
+def test_rmsnorm(numpy_snapshot, ts_state_dict, in_embeddings):
+    state_dict, _ = ts_state_dict
+    reference_weights = state_dict["layers.1.ln1.weight"]
+    d_model = reference_weights.shape[0]
+    rms_norm_layer = RMSNorm(
+        d_hidden=d_model, eps=1e-5, device="cpu", dtype=torch.float32
+    )
+    rms_norm_layer.load_state_dict({"weight": reference_weights})
+    actual_output = rms_norm_layer(in_embeddings)
+    numpy_snapshot.assert_match(actual_output, atol=1e-6)
+
+
+def test_layernorm(in_embeddings):
+    d_model = in_embeddings.shape[-1]
+    my_layernorm = LayerNorm(d_hidden=d_model, device="cpu", dtype=torch.float32)
+    reference_layernorm = torch.nn.LayerNorm(normalized_shape=d_model)
+    my_layernorm.load_state_dict(
+        {"weight": reference_layernorm.weight, "bias": reference_layernorm.bias}
+    )
+    my_output = my_layernorm(in_embeddings)
+    reference_output = reference_layernorm(in_embeddings)
+    assert torch.allclose(
+        my_output, reference_output, atol=1e-6
+    ), "Forward pass outputs do not match!"
 